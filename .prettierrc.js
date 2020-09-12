83643cb2e04209e7094f56082ebff97ca6126b37
ZumDeWald <zumdewald@gmail.com>
2020-09-12 13:50:45 -0500

    Modified Prettier RC

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,18 @@
 module.exports = {
+  arrowParens: "avoid",
   bracketSpacing: true,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  // Override any other rules you want
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
 };
