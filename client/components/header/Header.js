0d241298b0d1a416e542260c59123329f489675b
Søren Gravesen <S_gravesen92@hotmail.com>
2020-09-28 20:21:18 +0200

    CHIN-45 add basic mobile check

--- client/components/header/Header.js
+++ client/components/header/Header.js
@@ -4,48 +4,86 @@
 import React from 'react';
 import Head from "next/head";
 import Link from "next/link";
+import { breakpointsRaw } from '../../frontend-config';
 
 import { Wrapper, InnerWrapper, LogoWrapper, Logo, LogoText, Navbar, NavbarLink } from './styles';
 
-export default function Header({ children }) {
-  return (
-    <div>
-      <Head>
-        <title>Chingu Quiz App</title>
-      </Head>
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <Wrapper>
-        <InnerWrapper>
-          <Link href="/">
-            <LogoWrapper>
-              <Logo />
-              <LogoText>Chingu Quiz</LogoText>
-            </LogoWrapper>
-          </Link>
+    this.state = {
+      mobile: false,
+    }
 
-          <Navbar>
-            <Link href="/quizzes">
-              <NavbarLink>
-                Quiz
-              </NavbarLink>
+    this.checkPageSize = this.checkPageSize.bind(this);
+  }
+
+  componentDidMount() {
+    this.checkPageSize();
+    window.addEventListener('resize', this.checkPageSize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.checkPageSize);
+  }
+
+  checkPageSize() {
+    const { mobile } = this.state;
+
+    if (window.innerWidth >= breakpointsRaw('md') && mobile) {
+      this.setState({
+        mobile: false
+      })
+    } else if (window.innerWidth < breakpointsRaw('md') && !mobile) {
+      this.setState({
+        mobile: true
+      })
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div>
+        <Head>
+          <title>Chingu Quiz App</title>
+        </Head>
+
+        <Wrapper>
+          <InnerWrapper>
+            <Link href="/">
+              <LogoWrapper>
+                <Logo />
+                <LogoText>Chingu Quiz</LogoText>
+              </LogoWrapper>
             </Link>
 
-            <Link href="/contribute">
-              <NavbarLink>
-                Contribute
-              </NavbarLink>
-            </Link>
+            <Navbar>
+              <Link href="/quizzes">
+                <NavbarLink>
+                  Quiz
+                </NavbarLink>
+              </Link>
 
-            <Link href="/about">
-              <NavbarLink>
-                About Us
-              </NavbarLink>
-            </Link>
-          </Navbar>
-        </InnerWrapper>
-      </Wrapper>
+              <Link href="/contribute">
+                <NavbarLink>
+                  Contribute
+                </NavbarLink>
+              </Link>
 
-      <main>{children}</main>
-    </div>
-  );
+              <Link href="/about">
+                <NavbarLink>
+                  About Us
+                </NavbarLink>
+              </Link>
+            </Navbar>
+          </InnerWrapper>
+        </Wrapper>
+
+        <main>{children}</main>
+      </div>
+    );
+  }
 }
