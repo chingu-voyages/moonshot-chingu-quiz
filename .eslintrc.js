82e150fbf5de4cf105c15739a6ffe89fc3472887
Andrew Knox <silentk@Honeycrisp.local>
2020-09-29 22:06:15 -0700

    adding additional rules for linting
    turning on the eslint recommended rules
    integrating prettier extension with eslint
    allowing jsx in js files
    adjusting import/resolver settings as eslint wasn't able to find next.js
    imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  extends: ['airbnb'],
+  extends: ["eslint:recommended", "airbnb", "prettier"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
 };
