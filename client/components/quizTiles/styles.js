e2dbfb9e6c54a17ebab8bf47f97239820b8390c3
ZumDeWald <zumdewald@gmail.com>
2020-09-29 23:06:14 -0500

    Initial styled comps in place for quiz tiles

--- client/components/quizTiles/styles.js
+++ client/components/quizTiles/styles.js
@@ -10,4 +10,31 @@ export const tileWrapper = styled.section`
   margin: 0 auto;
 `;
 
-export const tileContainer = styled.div``;
+export const tileContainer = styled.div`
+  display: grid;
+  grid-gap: 0;
+  grid-template-columns: 130px 1fr;
+  grid-template-rows: 1fr;
+  place-items: center start;
+  place-content: center;
+  width: 300px;
+  height: 115px;
+  padding: 0;
+  margin: 0 auto;
+`;
+
+export const tileImage = styled.img`
+  width: 130px;
+  padding: 0;
+  margin: 0;
+`;
+
+export const tileBody = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 50px 1fr;
+  grid-gap: 0;
+  width: calc(100% - 130px);
+  padding: 0;
+  margin: 0;
+`;
