37a54cbb897b67e231b4980c08b65c2331626e53
ZumDeWald <zumdewald@gmail.com>
2020-09-29 22:22:56 -0500

    Started quizTiles and added general styles file

---
+++ client/components/quizTiles/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const tileWrapper = styled.section`
+  display: grid;
+  grid-gap: 15px;
+  grid-tempate-columns: repeat(auto-fit, minmax(300px, 1fr));
+  place-items: center;
+  place-content: center;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export const tileContainer = styled.div``;
---
+++ client/components/shared/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+// TEXT ELEMENTS //
+
+export const hugeText = styled.h2`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 69px;
+  line-height: 100%;
+`;
+
+export const heading1 = styled.h1`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 53px;
+  line-height: 120%;
+`;
+
+export const heading2 = styled.h2`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 39px;
+  line-height: 120%;
+`;
+
+export const heading3 = styled.h3`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 31px;
+  line-height: 36px;
+`;
+
+export const heading4 = styled.h4`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 25px;
+  line-height: 29px;
+`;
