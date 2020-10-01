169724dc91d34c6c0750fc67137f9e3cce74f727
Soren Gravesennode <S_gravesen92@hotmail.com>
2020-10-01 23:38:14 +0200

    #30 lint header files

--- client/components/header/Header.js
+++ client/components/header/Header.js
@@ -1,14 +1,22 @@
 /*
   This component is used in `/pages/_app.js` as a wrapper so it will remain mounted even when the 'page' changes
 */
-import React from 'react';
+import React from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { breakpointsRaw } from '../../frontend-config';
+import { breakpointsRaw } from "../../frontend-config";
 
-import MobileMenu from './MobileMenu';
+import MobileMenu from "./MobileMenu";
 
-import { Wrapper, InnerWrapper, LogoWrapper, Logo, LogoText, Navbar, NavbarLink } from './styles';
+import {
+  Wrapper,
+  InnerWrapper,
+  LogoWrapper,
+  Logo,
+  LogoText,
+  Navbar,
+  NavbarLink,
+} from "./styles";
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -16,8 +24,8 @@ export default class Header extends React.Component {
 
     this.state = {
       mobile: false,
-      mobileMenuActive: false
-    }
+      mobileMenuActive: false,
+    };
 
     this.checkPageSize = this.checkPageSize.bind(this);
     this.toggleMobileMenu = this.toggleMobileMenu.bind(this);
@@ -25,33 +33,33 @@ export default class Header extends React.Component {
 
   componentDidMount() {
     this.checkPageSize();
-    window.addEventListener('resize', this.checkPageSize);
+    window.addEventListener("resize", this.checkPageSize);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.checkPageSize);
+    window.removeEventListener("resize", this.checkPageSize);
   }
 
   checkPageSize() {
     const { mobile } = this.state;
 
-    if (window.innerWidth >= breakpointsRaw('md') && mobile) {
+    if (window.innerWidth >= breakpointsRaw("md") && mobile) {
       this.setState({
         mobile: false,
-        mobileMenuActive: false
-      })
-    } else if (window.innerWidth < breakpointsRaw('md') && !mobile) {
+        mobileMenuActive: false,
+      });
+    } else if (window.innerWidth < breakpointsRaw("md") && !mobile) {
       this.setState({
         mobile: true,
-        mobileMenuActive: false
-      })
+        mobileMenuActive: false,
+      });
     }
   }
 
   toggleMobileMenu() {
     this.setState(prevState => ({
-      mobileMenuActive: !prevState.mobileMenuActive
-    }))
+      mobileMenuActive: !prevState.mobileMenuActive,
+    }));
   }
 
   render() {
@@ -73,39 +81,31 @@ export default class Header extends React.Component {
               </LogoWrapper>
             </Link>
 
-            {
-              mobile ? (
-                <MobileMenu
-                  active={mobileMenuActive}
-                  toggleMobileMenu={this.toggleMobileMenu}
-                />
-              ) : (
-                  <Navbar>
-                    <Link href="/quizzes">
-                      <NavbarLink>
-                        Quiz
-                  </NavbarLink>
-                    </Link>
+            {mobile ? (
+              <MobileMenu
+                active={mobileMenuActive}
+                toggleMobileMenu={this.toggleMobileMenu}
+              />
+            ) : (
+              <Navbar>
+                <Link href="/quizzes">
+                  <NavbarLink>Quiz</NavbarLink>
+                </Link>
 
-                    <Link href="/contribute">
-                      <NavbarLink>
-                        Contribute
-                  </NavbarLink>
-                    </Link>
+                <Link href="/contribute">
+                  <NavbarLink>Contribute</NavbarLink>
+                </Link>
 
-                    <Link href="/about">
-                      <NavbarLink>
-                        About Us
-                  </NavbarLink>
-                    </Link>
-                  </Navbar>
-                )
-            }
+                <Link href="/about">
+                  <NavbarLink>About Us</NavbarLink>
+                </Link>
+              </Navbar>
+            )}
           </InnerWrapper>
-        </Wrapper >
+        </Wrapper>
 
         <main>{children}</main>
-      </div >
+      </div>
     );
   }
 }
