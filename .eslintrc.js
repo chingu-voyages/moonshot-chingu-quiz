c8520ee78058b25468cb066977a3c39e39d2adc4
silentkdev <silentk@silentk.dev>
2020-10-01 12:17:11 -0700

    more linting rule changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: ["eslint:recommended", "airbnb", "prettier"],
   rules: {
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/prop-types": 0,
+    "jsx-a11y/anchor-is-valid": 0,
   },
   settings: {
     "import/resolver": {
