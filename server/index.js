e396ac30ce948f93d8c206c8a0884f7b2d66bbf1
silentkdev <silentk@silentk.dev>
2020-10-01 20:59:29 -0700

    moving "constant" toward the top of the file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is up on port ${PORT} `);
