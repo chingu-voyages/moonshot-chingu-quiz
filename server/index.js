423960691c1eb984ccc216ca06581460fbaa074d
silentkdev <silentk@silentk.dev>
2020-10-02 00:39:19 -0700

    mounting initial api vi router to express app

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const apiV1 = require("./routes/apiV1");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// apiV1 router
+app.use("/api/v1", apiV1);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
