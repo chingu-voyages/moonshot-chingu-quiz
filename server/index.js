d2e77d28e37d19ed426fa9c1e7754d00e0743555
silentkdev <silentk@silentk.dev>
2020-10-01 21:50:22 -0700

    adding cors dependency so client can make requests

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
