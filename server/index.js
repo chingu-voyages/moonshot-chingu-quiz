21e8935143c15d9f947c86e874902151192ba3ad
silentkdev <silentk@silentk.dev>
2020-10-01 21:47:22 -0700

    adding morgan logging middleware to express app

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
