289ea6a1fafebed21d11e5cd1d2178d44031f09d
silentkdev <silentk@silentk.dev>
2020-10-01 21:17:35 -0700

    adding body-parser middleware to express app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
