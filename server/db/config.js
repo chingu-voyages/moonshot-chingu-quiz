bb429b3ddce9da457a155f5b06257e19789aa944
silentkdev <silentk@silentk.dev>
2020-10-02 00:33:26 -0700

    cors and morgan dependecies

--- server/db/config.js
+++ server/db/config.js
@@ -1,11 +1,7 @@
-const { Pool } = require("pg");
-
-const pool = new Pool({
+module.exports = {
   username: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-});
-
-module.exports = pool;
+};
