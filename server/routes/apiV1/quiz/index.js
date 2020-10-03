6b4c8a6840209a8ba957c4f10ed05b555b1d305b
silentkdev <silentk@silentk.dev>
2020-10-03 00:32:09 -0700

    adding get handler to the quiz router

--- server/routes/apiV1/quiz/index.js
+++ server/routes/apiV1/quiz/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const getHandler = require("./get");
 const postHandler = require("./post");
 
 const router = express.Router();
 
+router.get("/", getHandler);
 router.post("/", postHandler);
 
 module.exports = router;
