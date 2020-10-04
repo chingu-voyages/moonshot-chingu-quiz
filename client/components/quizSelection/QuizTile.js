7dc12741469f6c4d32454897c02861bf80f58497
ZumDeWald <zumdewald@gmail.com>
2020-10-04 16:42:38 -0500

    Swapped grid for flex; updated tile style

--- client/components/quizSelection/QuizTile.js
+++ client/components/quizSelection/QuizTile.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { TileContainer, TileImage, TileBody } from "./styles";
+import { TileContainer } from "./styles";
 import { Heading4, TextBodySmall } from "../shared/styles";
 
 export default function QuizTile({ quizData }) {
   return (
     <TileContainer>
-      <TileImage src="/CSS3_Logo_200.png" />
-      <TileBody>
-        <Heading4>{quizData.title}</Heading4>
-        <TextBodySmall>{quizData.description}</TextBodySmall>
-      </TileBody>
+      <Heading4>{quizData.title}</Heading4>
+      <TextBodySmall>{quizData.description}</TextBodySmall>
     </TileContainer>
   );
 }
--- client/components/quizSelection/styles.js
+++ client/components/quizSelection/styles.js
@@ -12,11 +12,10 @@ export const QuizzesHeader = styled.section`
 `;
 
 export const TileSection = styled.section`
-  display: grid;
-  grid-gap: 15px;
-  grid-template-columns: repeat(auto-fit, minmax(375px, 1fr));
-  place-items: start;
-  place-content: center;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+  align-items: center;
   width: 100%;
   margin: 10px auto;
 
@@ -26,49 +25,29 @@ export const TileSection = styled.section`
   }
 `;
 
-export const TileContainer = styled.button`
-  display: grid;
-  grid-gap: 2px;
-  grid-template-columns: 100px 1fr;
-  grid-template-rows: 1fr;
-  place-items: center;
-  place-content: center;
+export const TileContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: start;
+  align-items: start;
   width: 100%;
   height: 105px;
-  padding: 0;
-  margin: 10px 0;
+  padding: 12px;
+  margin: 10px auto;
   background: transparent;
-  border: none;
+  border-left: 10px solid ${props => props.theme.colors.darkGreen};
   border-radius: 5px;
-  box-shadow: 3px 1px 10px #c4c4c4;
+  box-shadow: 2px 1px 10px #c4c4c4;
   transition-duration: 350ms;
 
   &:hover {
-    box-shadow: 5px 3px 18px #c4c4c4;
+    box-shadow: 10px 3px 18px #c4c4c4;
     cursor: pointer;
   }
 
   @media only screen and (min-width: ${breakpoint("xs")}) {
     width: 375px;
   }
-`;
-
-export const TileImage = styled.img`
-  width: 50px;
-  padding: 0;
-  margin: 0;
-`;
-
-export const TileBody = styled.div`
-  place-self: start;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 30px 1fr;
-  grid-gap: 0;
-  width: 100%;
-  padding: 0 5px 0 0;
-  margin: 10px 0 0 0;
-  text-align: left;
 `;
 
 export const TopicSelectionContainer = styled.section`
