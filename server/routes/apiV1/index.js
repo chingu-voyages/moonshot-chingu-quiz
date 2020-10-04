15a642068a8d4528db4cba8bd6cfb91646f3fd62
silentkdev <silentk@silentk.dev>
2020-10-03 17:07:09 -0700

    adding answer router to express top level router

--- server/routes/apiV1/index.js
+++ server/routes/apiV1/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const quizRouter = require("./quiz");
+const answerRouter = require("./answer");
 
 const router = express.Router();
 
 router.use("/quiz", quizRouter);
+router.use("/answer", answerRouter);
 
 module.exports = router;
