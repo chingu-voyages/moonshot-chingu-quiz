ae343dd7b1036b35b8ac32917090e36b877e02cb
silentkdev <silentk@silentk.dev>
2020-10-03 17:08:26 -0700

    initial GET route for answer end point

---
+++ server/routes/apiV1/answer/get.js
@@ -0,0 +1,50 @@
+const db = require("../../../db");
+
+module.exports = async (req, res) => {
+  try {
+    const constructedResponse = [];
+    const { answerIds } = req.query;
+
+    if (!Array.isArray(answerIds) || answerIds.length < 1) {
+      return res.status(400).json({
+        message:
+          "Payload should be an array in the form of URL parameters." +
+          "Please format as such: " +
+          "/api/v1/answer?answerIds=ID1HERE&answerIds=ID2HERE...&answerIds=IDNHERE",
+      });
+    }
+
+    await Promise.all(
+      answerIds.map(async answerId => {
+        const {
+          rows: [{ question: questionId, is_correct: isCorrect }],
+        } = await db.query(
+          "SELECT question, is_correct FROM answer WHERE id = $1",
+          [answerId]
+        );
+
+        const {
+          rows: [{ explanation, resources }],
+        } = await db.query(
+          "SELECT explanation, resources FROM question WHERE id = $1",
+          [questionId]
+        );
+
+        constructedResponse.push({
+          id: answerId,
+          isCorrect,
+          explanation,
+          resources,
+        });
+      })
+    );
+
+    return res.json(constructedResponse);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
+
+  return null;
+};
