26169da52f8f78da328fc777e3c02b66be358e4e
ZumDeWald <zumdewald@gmail.com>
2020-10-05 00:23:23 -0500

    Added tags to quiz tiles

--- client/components/quizSelection/QuizTile.js
+++ client/components/quizSelection/QuizTile.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { TileContainer } from "./styles";
-import { Heading4, TextBodySmall } from "../shared/styles";
+import { Heading4, TextBodySmall, TextBodyMicro } from "../shared/styles";
+import { TileContainer, TileTagContainer, QuizTileTag } from "./styles";
 
 export default function QuizTile({ quizData }) {
   return (
     <TileContainer>
-      <Heading4>{quizData.title}</Heading4>
-      <TextBodySmall>{quizData.description}</TextBodySmall>
+      <div>
+        <Heading4>{quizData.title}</Heading4>
+        <TextBodySmall>{quizData.description}</TextBodySmall>
+      </div>
+      <TileTagContainer>
+        {quizData.tags.map(tag => (
+          <QuizTileTag>
+            <TextBodyMicro>{tag}</TextBodyMicro>
+          </QuizTileTag>
+        ))}
+      </TileTagContainer>
     </TileContainer>
   );
 }
--- client/components/quizSelection/styles.js
+++ client/components/quizSelection/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { breakpoint } from "../../frontend-config";
+import { PrimaryButton } from "../shared/styles";
 
 export const QuizzesHeader = styled.section`
   display: flex;
@@ -28,16 +29,16 @@ export const TileSection = styled.section`
 export const TileContainer = styled.div`
   display: flex;
   flex-flow: column nowrap;
-  justify-content: start;
+  justify-content: space-between;
   align-items: start;
   width: 100%;
-  height: 105px;
-  padding: 12px;
-  margin: 10px auto;
+  height: 130px;
+  padding: 8px 12px;
+  margin: 20px;
   background: transparent;
   border-left: 10px solid ${props => props.theme.colors.darkGreen};
   border-radius: 5px;
-  box-shadow: 2px 1px 10px #c4c4c4;
+  box-shadow: 1px 1px 10px #c4c4c4;
   transition-duration: 350ms;
 
   &:hover {
@@ -48,6 +49,11 @@ export const TileContainer = styled.div`
   @media only screen and (min-width: ${breakpoint("xs")}) {
     width: 375px;
   }
+`;
+
+export const TileTagContainer = styled.span`
+  display: flex;
+  fled-flow: row nowrap;
 `;
 
 export const TopicSelectionContainer = styled.section`
@@ -75,4 +81,9 @@ export const TopicSelectionItem = styled.li`
   width: max-content;
   padding: 0;
   margin-right: 15px;
+`;
+
+export const QuizTileTag = styled(PrimaryButton)`
+  padding: 1px 5px;
+  margin: 5px 5px 0 0;
 `;
