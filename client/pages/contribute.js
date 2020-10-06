26432580ecf7ce5fa164dc54cecf1ab53dce84b4
lily-law <lilylaw.dev@gmail.com>
2020-10-06 16:43:48 +0100

    Add contribute page

---
+++ client/pages/contribute.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../components/header/Header";
+
+export default function Home() {
+  return <></>;
+}
+
+// Tells NextJS this page should use the Header setup in '_app'
+Home.Header = Header;
