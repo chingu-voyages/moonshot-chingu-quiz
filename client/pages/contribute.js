042db73852e93037b7781c63f660b3795828796a
lily-law <lilylaw.dev@gmail.com>
2020-10-06 16:49:30 +0100

    Add resources to contribute page

--- client/pages/contribute.js
+++ client/pages/contribute.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/header/Header";
 import Hero from "../components/contributepage/HeroSection";
 import Info from "../components/contributepage/InfoSection";
+import Resources from "../components/contributepage/ResourcesSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Info />
+      <Resources />
     </>
   );
 }
