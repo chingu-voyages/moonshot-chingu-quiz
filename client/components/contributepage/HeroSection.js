e0092adf99f1cb8d67bb99964b71cc3630a06bf9
lily-law <lilylaw.dev@gmail.com>
2020-10-06 16:48:34 +0100

    Add hero to contribute page

---
+++ client/components/contributepage/HeroSection.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import { breakpoint } from "../../frontend-config";
+
+const Wrapper = styled.div`
+  background: ${props => props.theme.colors.grey};
+  padding: 45px 25px 65px;
+
+  @media (min-width: ${breakpoint("lg")}) {
+    padding: 115px 25px 190px;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  max-width: 730px;
+  margin: 0 auto;
+`;
+
+const Headline = styled.h1`
+  font-size: 25px;
+  line-height: 30px;
+  font-weight: 700;
+  letter-spacing: 0.5px;
+  text-align: center;
+  color: ${props => props.theme.colors.midGreen};
+  margin-bottom: 32px;
+
+  @media (min-width: ${breakpoint("md")}) {
+    font-size: 40px;
+    line-height: 44px;
+  }
+
+  @media (min-width: ${breakpoint("lg")}) {
+    font-size: 53px;
+    line-height: 63px;
+  }
+`;
+
+const HeroSection = () => {
+  return (
+    <Wrapper>
+      <ContentWrapper>
+        <Headline>How To Contribute</Headline>
+      </ContentWrapper>
+    </Wrapper>
+  );
+};
+
+export default HeroSection;
--- client/pages/contribute.js
+++ client/pages/contribute.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "../components/header/Header";
+import Hero from "../components/contributepage/HeroSection";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
-
 // Tells NextJS this page should use the Header setup in '_app'
 Home.Header = Header;
