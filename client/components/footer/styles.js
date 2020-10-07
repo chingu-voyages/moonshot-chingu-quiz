37a5712bf47c20d70561b3590dff9ed59ca2b54e
Christin Mikolai <mail@christinmikolai.de>
2020-10-07 22:20:20 +0200

    replace colors with theme props

--- client/components/footer/styles.js
+++ client/components/footer/styles.js
@@ -3,7 +3,7 @@ import { breakpoint } from "../../frontend-config";
 
 export const Wrapper = styled.div`
   width: 100%;
-  background-color: #333;
+  background-color: ${props => props.theme.colors.grey};
   height: 66px;
   display: flex;
   align-items: center;
@@ -13,10 +13,10 @@ export const ContentWrapper = styled.div`
   width: ${breakpoint("maxWidth")};
   max-width: calc(100% - 70px);
   margin: 0 auto;
-  color: #fff;
+  color: ${props => props.theme.colors.light};
 `;
 
 export const HighlightLink = styled.span`
-  color: #18e28c;
+  color: ${props => props.theme.colors.midGreen};
   margin-left: 5px;
 `;
