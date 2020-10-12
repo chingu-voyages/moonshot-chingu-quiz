5ecfaf464ed34f300d0897c952b48b059cb25703
silentkdev <silentk@silentk.dev>
2020-10-12 00:06:29 -0700

    tags get router controller

---
+++ server/controllers/apiV1/tags/get.js
@@ -0,0 +1,12 @@
+const { select } = require("../../../db/queries/tags");
+
+module.exports = async (req, res) => {
+  try {
+    const tags = await select();
+    res.json(tags);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
+};
