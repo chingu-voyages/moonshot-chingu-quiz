e6b5c9df58613b23ae18ad7bd1748cac9e3c8247
silentkdev <silentk@silentk.dev>
2020-10-12 00:03:52 -0700

    adding tags router to express app main router

--- server/routes/apiV1/index.js
+++ server/routes/apiV1/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const quiz = require("./quiz");
 const answer = require("./answer");
 const subject = require("./subject");
+const tags = require("./tags");
 
 const router = express.Router();
 
 router.use("/quiz", quiz);
 router.use("/answer", answer);
 router.use("/subject", subject);
+router.use("/tags", tags);
 
 module.exports = router;
