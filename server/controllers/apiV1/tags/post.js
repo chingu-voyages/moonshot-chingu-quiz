03946e6548482eec62d6e408cf8ecd617fedaf7f
silentkdev <silentk@silentk.dev>
2020-10-12 00:05:43 -0700

    tags post router controller

---
+++ server/controllers/apiV1/tags/post.js
@@ -0,0 +1,16 @@
+const { insert } = require("../../../db/queries/tags");
+
+module.exports = async (req, res) => {
+  try {
+    const { title } = req.body;
+    if (!title) {
+      return res.status(400).json({ message: "title property is required" });
+    }
+    const tag = await insert({ title });
+    return res.json(tag);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    return res.status(500).json({ message: err.message });
+  }
+};
