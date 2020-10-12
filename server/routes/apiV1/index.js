cab61a004acde6fbae3b59e6332dd37dcc3c92e2
silentkdev <silentk@silentk.dev>
2020-10-11 22:54:20 -0700

    adding subject router to app router

--- server/routes/apiV1/index.js
+++ server/routes/apiV1/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const quiz = require("./quiz");
 const answer = require("./answer");
+const subject = require("./subject");
 
 const router = express.Router();
 
 router.use("/quiz", quiz);
 router.use("/answer", answer);
+router.use("/subject", subject);
 
 module.exports = router;
