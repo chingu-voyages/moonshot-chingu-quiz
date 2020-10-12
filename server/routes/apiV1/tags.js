bc533a7177245fcacf04a626244bf92b49ac181c
silentkdev <silentk@silentk.dev>
2020-10-12 00:03:03 -0700

    initial tags router

---
+++ server/routes/apiV1/tags.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { get, getById, post } = require("../../controllers/apiV1/tags");
+
+const router = express.Router();
+
+router.get("/", get);
+router.get("/:id", getById);
+router.post("/", post);
+
+module.exports = router;
