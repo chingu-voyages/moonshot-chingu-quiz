6551e90a1f9b65040672351d9be74470ef00ce3c
silentkdev <silentk@silentk.dev>
2020-10-11 20:44:22 -0700

    update schema

--- quiz_db.sql
+++ quiz_db.sql
@@ -1,104 +1,209 @@
 --
 -- PostgreSQL database dump
 --
+
+-- Dumped from database version 13.0 (Debian 13.0-1.pgdg100+1)
+-- Dumped by pg_dump version 13.0 (Debian 13.0-1.pgdg100+1)
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
+SET row_security = off;
+
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
+-- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
 --
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
---
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
---
--- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner:
---
+
 CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+
 --
--- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner:
+-- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: 
 --
+
 COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
+
+
 --
--- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner:
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
 --
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
 --
--- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner:
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
 --
+
 COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
-SET search_path = public,
-    pg_catalog;
+
+
 SET default_tablespace = '';
-SET default_with_oids = false;
+
+SET default_table_access_method = heap;
+
 --
--- Name: answer; Type: TABLE; Schema: public; Owner: docker; Tablespace:
+-- Name: answer; Type: TABLE; Schema: public; Owner: docker
 --
-CREATE TABLE answer (
-    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+
+CREATE TABLE public.answer (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
     question uuid NOT NULL,
     prompt character varying(1000) NOT NULL,
-    is_correct boolean NOT NULL
+    is_correct boolean NOT NULL,
+    quiz uuid NOT NULL
 );
+
+
 ALTER TABLE public.answer OWNER TO docker;
+
 --
--- Name: question; Type: TABLE; Schema: public; Owner: docker; Tablespace:
+-- Name: question; Type: TABLE; Schema: public; Owner: docker
 --
-CREATE TABLE question (
-    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+
+CREATE TABLE public.question (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
     quiz uuid NOT NULL,
     prompt character varying(1000) NOT NULL
 );
+
+
 ALTER TABLE public.question OWNER TO docker;
+
 --
--- Name: quiz; Type: TABLE; Schema: public; Owner: docker; Tablespace:
+-- Name: quiz; Type: TABLE; Schema: public; Owner: docker
 --
-CREATE TABLE quiz (
+
+CREATE TABLE public.quiz (
     description character varying(1000) NOT NULL,
     title character varying(240) NOT NULL,
-    id uuid DEFAULT uuid_generate_v4() NOT NULL,
-    subject character varying [] NOT NULL,
-    tags character varying [] NOT NULL
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    subject uuid[] NOT NULL,
+    tags uuid[] NOT NULL
 );
+
+
 ALTER TABLE public.quiz OWNER TO docker;
+
 --
--- Name: answer_pk; Type: CONSTRAINT; Schema: public; Owner: docker; Tablespace:
+-- Name: subject; Type: TABLE; Schema: public; Owner: docker
 --
-ALTER TABLE ONLY answer
-ADD CONSTRAINT answer_pk PRIMARY KEY (id);
+
+CREATE TABLE public.subject (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    title character varying(240) NOT NULL
+);
+
+
+ALTER TABLE public.subject OWNER TO docker;
+
 --
--- Name: question_pk; Type: CONSTRAINT; Schema: public; Owner: docker; Tablespace:
+-- Name: tags; Type: TABLE; Schema: public; Owner: docker
 --
-ALTER TABLE ONLY question
-ADD CONSTRAINT question_pk PRIMARY KEY (id);
+
+CREATE TABLE public.tags (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    title character varying(240) NOT NULL
+);
+
+
+ALTER TABLE public.tags OWNER TO docker;
+
 --
--- Name: quiz_pk; Type: CONSTRAINT; Schema: public; Owner: docker; Tablespace:
+-- Data for Name: answer; Type: TABLE DATA; Schema: public; Owner: docker
 --
-ALTER TABLE ONLY quiz
-ADD CONSTRAINT quiz_pk PRIMARY KEY (id);
+
+COPY public.answer (id, question, prompt, is_correct, quiz) FROM stdin;
+\.
+
+
 --
--- Name: answer_fk; Type: FK CONSTRAINT; Schema: public; Owner: docker
+-- Data for Name: question; Type: TABLE DATA; Schema: public; Owner: docker
 --
-ALTER TABLE ONLY answer
-ADD CONSTRAINT answer_fk FOREIGN KEY (question) REFERENCES question(id);
+
+COPY public.question (id, quiz, prompt) FROM stdin;
+\.
+
+
 --
--- Name: question_fk; Type: FK CONSTRAINT; Schema: public; Owner: docker
+-- Data for Name: quiz; Type: TABLE DATA; Schema: public; Owner: docker
 --
-ALTER TABLE ONLY question
-ADD CONSTRAINT question_fk FOREIGN KEY (quiz) REFERENCES quiz(id);
+
+COPY public.quiz (description, title, id, subject, tags) FROM stdin;
+\.
+
+
 --
--- Name: public; Type: ACL; Schema: -; Owner: docker
+-- Data for Name: subject; Type: TABLE DATA; Schema: public; Owner: docker
 --
-REVOKE ALL ON SCHEMA public
-FROM PUBLIC;
-REVOKE ALL ON SCHEMA public
-FROM docker;
-GRANT ALL ON SCHEMA public TO docker;
-GRANT ALL ON SCHEMA public TO PUBLIC;
+
+COPY public.subject (id, title) FROM stdin;
+\.
+
+
+--
+-- Data for Name: tags; Type: TABLE DATA; Schema: public; Owner: docker
+--
+
+COPY public.tags (id, title) FROM stdin;
+\.
+
+
+--
+-- Name: answer answer_pk; Type: CONSTRAINT; Schema: public; Owner: docker
+--
+
+ALTER TABLE ONLY public.answer
+    ADD CONSTRAINT answer_pk PRIMARY KEY (id);
+
+
+--
+-- Name: question question_pk; Type: CONSTRAINT; Schema: public; Owner: docker
+--
+
+ALTER TABLE ONLY public.question
+    ADD CONSTRAINT question_pk PRIMARY KEY (id);
+
+
+--
+-- Name: quiz quiz_pk; Type: CONSTRAINT; Schema: public; Owner: docker
+--
+
+ALTER TABLE ONLY public.quiz
+    ADD CONSTRAINT quiz_pk PRIMARY KEY (id);
+
+
+--
+-- Name: answer answer_fk_question; Type: FK CONSTRAINT; Schema: public; Owner: docker
+--
+
+ALTER TABLE ONLY public.answer
+    ADD CONSTRAINT answer_fk_question FOREIGN KEY (question) REFERENCES public.question(id);
+
+
+--
+-- Name: answer answer_fk_quiz; Type: FK CONSTRAINT; Schema: public; Owner: docker
+--
+
+ALTER TABLE ONLY public.answer
+    ADD CONSTRAINT answer_fk_quiz FOREIGN KEY (quiz) REFERENCES public.quiz(id);
+
+
+--
+-- Name: question question_fk; Type: FK CONSTRAINT; Schema: public; Owner: docker
+--
+
+ALTER TABLE ONLY public.question
+    ADD CONSTRAINT question_fk FOREIGN KEY (quiz) REFERENCES public.quiz(id);
+
+
 --
 -- PostgreSQL database dump complete
 --
+
