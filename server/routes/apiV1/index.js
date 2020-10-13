0c8def151ceaa4ce1fe973727c617f9febb6aab4
silentkdev <silentk@silentk.dev>
2020-10-13 14:51:25 -0700

    removing import of old answers route

--- server/routes/apiV1/index.js
+++ server/routes/apiV1/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const quiz = require("./quiz");
-const answer = require("./answer");
 const subject = require("./subject");
 const tag = require("./tag");
 
 const router = express.Router();
 
 router.use("/quiz", quiz);
-router.use("/answer", answer);
 router.use("/subject", subject);
 router.use("/tag", tag);
 
