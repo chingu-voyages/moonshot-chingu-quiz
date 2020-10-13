e521ce5f116f8384ceac04673d22f077b64a5a47
silentkdev <silentk@silentk.dev>
2020-10-12 21:00:20 -0700

    returning only quiz table colums refactor

--- server/controllers/apiV1/quiz/get.js
+++ server/controllers/apiV1/quiz/get.js
@@ -1,59 +1,10 @@
-const { select: selectQuiz } = require("../../../db/queries/quiz");
-const {
-  selectByQuizId: selectQuestionByQuizId,
-} = require("../../../db/queries/question");
-const {
-  selectByQuestionId: selectAnswerByQuestionId,
-} = require("../../../db/queries/answer");
+const { select } = require("../../../db/queries/quiz");
 
 module.exports = async (req, res) => {
   try {
-    const constructedResponse = [];
+    const quizzes = await select();
 
-    const quizResults = await selectQuiz();
-
-    await Promise.all(
-      quizResults.map(async (quizResult, i) => {
-        constructedResponse.push(quizResult);
-        constructedResponse[i].questions = [];
-
-        const { id: quizId } = quizResult;
-        const questionResults = await selectQuestionByQuizId(quizId);
-
-        await Promise.all(
-          questionResults.map(
-            async (
-              {
-                id: questionId,
-                prompt: questionPrompt,
-                explanation,
-                resources,
-              },
-              j
-            ) => {
-              constructedResponse[i].questions.push({
-                id: questionId,
-                prompt: questionPrompt,
-                explanation,
-                resources,
-                answers: [],
-              });
-
-              const answerResults = await selectAnswerByQuestionId(questionId);
-
-              answerResults.forEach(({ id: answerId, prompt: answerPrompt }) =>
-                constructedResponse[i].questions[j].answers.push({
-                  id: answerId,
-                  prompt: answerPrompt,
-                })
-              );
-            }
-          )
-        );
-      })
-    );
-
-    res.json(constructedResponse);
+    res.json(quizzes);
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(err);
