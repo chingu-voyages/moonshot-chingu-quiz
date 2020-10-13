d441b5f56e85edf1947b57a71a89b6edd31590a9
silentkdev <silentk@silentk.dev>
2020-10-13 14:49:26 -0700

    removing old answer route

--- server/routes/apiV1/answer.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { get } = require("../../controllers/apiV1/answer");
-
-const router = express.Router();
-
-router.get("/", get);
-
-module.exports = router;
