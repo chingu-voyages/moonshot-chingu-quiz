9f35538fe1346dc0e42da4be9a211512d20d9e12
silentkdev <silentk@silentk.dev>
2020-10-12 23:40:26 -0700

    adjusting parameter to be an id instead of individual arguments

--- server/db/queries/question/select.js
+++ server/db/queries/question/select.js
@@ -1,13 +1,13 @@
 const db = require("../..");
 
-exports.selectById = async id => {
+exports.selectById = async ({ id }) => {
   const { rows } = await db.query("SELECT * FROM question WHERE id = $1", [id]);
   return rows;
 };
 
-exports.selectByQuizId = async quizId => {
+exports.selectByQuizId = async ({ id }) => {
   const { rows } = await db.query("SELECT * FROM question WHERE quiz = $1", [
-    quizId,
+    id,
   ]);
   return rows;
 };
