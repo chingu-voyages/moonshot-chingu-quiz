c35c3103e39f08f1c33d8a60392cb9247a77dc08
silentkdev <silentk@silentk.dev>
2020-10-13 14:51:06 -0700

    adding new answer conrollers to quiz router

--- server/routes/apiV1/quiz.js
+++ server/routes/apiV1/quiz.js
@@ -4,13 +4,19 @@ const {
   getByQuizId: getQuestionByQuizId,
   post: postQuestion,
 } = require("../../controllers/apiV1/question");
+const {
+  getByQuestionId: getAnswerByQuestionId,
+  post: postAnswer,
+} = require("../../controllers/apiV1/answer");
 
 const router = express.Router();
 
 router.get("/", get);
 router.get("/:id", getById);
 router.get("/:id/question", getQuestionByQuizId);
+router.get("/:id/question/:questionId/answer", getAnswerByQuestionId);
 router.post("/", post);
 router.post("/:id/question", postQuestion);
+router.post("/:id/question/:questionId/answer", postAnswer);
 
 module.exports = router;
