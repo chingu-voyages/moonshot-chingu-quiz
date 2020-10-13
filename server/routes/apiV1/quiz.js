cfa67330bb9b588f27f2d2ec7b6ac31a4efe8552
silentkdev <silentk@silentk.dev>
2020-10-12 23:38:20 -0700

    adding question routes to quiz router

--- server/routes/apiV1/quiz.js
+++ server/routes/apiV1/quiz.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const { get, getById, post } = require("../../controllers/apiV1/quiz");
+const {
+  getByQuizId: getQuestionByQuizId,
+  post: postQuestion,
+} = require("../../controllers/apiV1/question");
 
 const router = express.Router();
 
 router.get("/", get);
 router.get("/:id", getById);
+router.get("/:id/question", getQuestionByQuizId);
 router.post("/", post);
+router.post("/:id/question", postQuestion);
 
 module.exports = router;
