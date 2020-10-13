9ec98d02d150a10115f081ce65f417cddf0d0d87
silentkdev <silentk@silentk.dev>
2020-10-12 22:20:03 -0700

    selectById quiz db query

--- server/db/queries/quiz/select.js
+++ server/db/queries/quiz/select.js
@@ -4,3 +4,8 @@ exports.select = async () => {
   const { rows } = await db.query("SELECT * FROM quiz");
   return rows;
 };
+
+exports.selectById = async ({ id }) => {
+  const { rows } = await db.query("SELECT * FROM quiz WHERE id = $1", [id]);
+  return rows;
+};
