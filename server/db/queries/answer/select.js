2ac44d62e34ef1f64a117b183f6f70d9052006c4
silentkdev <silentk@silentk.dev>
2020-10-13 14:41:44 -0700

    refactoring select queries for answer table

--- server/db/queries/answer/select.js
+++ server/db/queries/answer/select.js
@@ -1,13 +1,16 @@
 const db = require("../..");
 
-exports.selectById = async id => {
+exports.selectById = async ({ id }) => {
   const { rows } = await db.query("SELECT * FROM answer WHERE id = $1", [id]);
   return rows;
 };
 
-exports.selectByQuestionId = async questionId => {
-  const { rows } = await db.query("SELECT * FROM answer WHERE question = $1", [
+exports.selectByQuizAndQuestionId = async ({ quizId, questionId }) => {
+  const {
+    rows,
+  } = await db.query("SELECT * FROM answer WHERE question = $1 AND quiz = $2", [
     questionId,
+    quizId,
   ]);
   return rows;
 };
