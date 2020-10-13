bb1006691d181183d56385ce8343700e09ea941c
silentkdev <silentk@silentk.dev>
2020-10-12 22:22:13 -0700

    adding getById controller to quiz router

--- server/routes/apiV1/quiz.js
+++ server/routes/apiV1/quiz.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { get, post } = require("../../controllers/apiV1/quiz");
+const { get, getById, post } = require("../../controllers/apiV1/quiz");
 
 const router = express.Router();
 
 router.get("/", get);
+router.get("/:id", getById);
 router.post("/", post);
 
 module.exports = router;
