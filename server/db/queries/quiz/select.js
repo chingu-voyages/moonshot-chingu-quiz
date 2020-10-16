1c7e64eb28a74060c361ecb1787e629a7ab372f7
dpajek <dan.pajek@gmail.com>
2020-10-15 20:34:53 -0400

    <subject> Random Quizzes
    <body> Added route/controller/query to get X random quizzes
    
    Resolves #74

--- server/db/queries/quiz/select.js
+++ server/db/queries/quiz/select.js
@@ -5,6 +5,13 @@ exports.select = async () => {
   return rows;
 };
 
+exports.selectRandomQuizzes = async ({ num }) => {
+  const {
+    rows,
+  } = await db.query("SELECT * FROM quiz ORDER BY RANDOM() LIMIT $1", [num]);
+  return rows;
+};
+
 exports.selectById = async ({ id }) => {
   const { rows } = await db.query("SELECT * FROM quiz WHERE id = $1", [id]);
   return rows;
