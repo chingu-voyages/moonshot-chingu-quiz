0653016a6d7ef7aec22d4c3703c5c16c0edfe753
silentkdev <silentk@silentk.dev>
2020-10-29 21:13:46 -0700

    adding get quiz by subject id controller to quiz controller module

--- server/controllers/apiV1/quiz/index.js
+++ server/controllers/apiV1/quiz/index.js
@@ -1,11 +1,13 @@
 const get = require("./get");
 const getRandomQuizzes = require("./getRandomQuizzes");
 const getById = require("./getById");
+const getBySubjectId = require("./get");
 const post = require("./post");
 
 module.exports = {
   get,
   getRandomQuizzes,
   getById,
+  getBySubjectId,
   post,
 };
