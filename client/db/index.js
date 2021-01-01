ce08e9f69ea51bca755277b4b1d9e6064c63615f
Andrew Knox <andrewknox@silentk.dev>
2021-01-01 14:05:10 -0800

    postgres connection and logging

---
+++ client/db/index.js
@@ -0,0 +1,20 @@
+import { Pool } from "pg";
+import config from "./config";
+
+const pool = new Pool(config);
+
+export default {
+  async query(text, params) {
+    const client = await pool.connect();
+    try {
+      const start = Date.now();
+      const result = await client.query(text, params);
+      const duration = Date.now() - start;
+      // eslint-disable-next-line no-console
+      console.log("executed query", { text, duration, rows: result.rowCount });
+      return result;
+    } finally {
+      client.release();
+    }
+  },
+};
