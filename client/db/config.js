8d5a64d77d91ae181c5cac54b2d9fe78b4d34787
Andrew Knox <andrewknox@silentk.dev>
2021-01-01 14:04:58 -0800

    postgres config

---
+++ client/db/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  username: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+};
