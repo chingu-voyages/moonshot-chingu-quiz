1a3d44ff440c3409a04aa38b815f7fd05eae7f37
Andrew Knox <andrewknox@silentk.dev>
2021-02-12 14:15:15 -0800

    adding disabled button for next question button

--- client/components/quizSingle/NextQuestionBtn.tsx
+++ client/components/quizSingle/NextQuestionBtn.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { Heading4 } from "../shared/styles";
-import { NextQuestionBtnStyled } from "./styles";
+import { NextQuestionBtnStyled, DisabledNextQuestionBtnStyled } from "./styles";
 
-export default function NextQuestionBtn() {
-  return (
+interface NextQuestionBtnProps {
+  disabled?: Boolean;
+}
+
+export default function NextQuestionBtn({ disabled }: NextQuestionBtnProps) {
+  return disabled ? (
+    <DisabledNextQuestionBtnStyled>
+      <Heading4>NEXT</Heading4>
+    </DisabledNextQuestionBtnStyled>
+  ) : (
     <NextQuestionBtnStyled>
       <Heading4>NEXT</Heading4>
     </NextQuestionBtnStyled>
