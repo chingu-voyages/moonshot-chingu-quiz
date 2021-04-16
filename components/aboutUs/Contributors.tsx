2d28a2b03590baaf4f42fb2ff966be6951fa7876
Søren Gravesen <sgr@cybot.com>
2021-04-17 01:40:38 +0200

    Adjust tab spacing #3

--- components/aboutUs/Contributors.tsx
+++ components/aboutUs/Contributors.tsx
@@ -33,29 +33,29 @@ const Contributors = () => {
   const [contributors, setContributors] = useState<any[]>([]);
 
   useEffect(() => {
-	fetch("https://api.github.com/repos/chingu-voyages/moonshot-chingu-quiz/contributors")
-	  .then(response => response.json())
-	  .then(json => setContributors(json));
+    fetch("https://api.github.com/repos/chingu-voyages/moonshot-chingu-quiz/contributors")
+      .then(response => response.json())
+      .then(json => setContributors(json));
   }, []);
 
   return contributors && contributors.length > 0 ? (
     <Wrapper>
       <HeadingGroup>
-	    <MidGreenBar />
-		<Headline>Contributors</Headline>
-	  </HeadingGroup>
+        <MidGreenBar />
+        <Headline>Contributors</Headline>
+      </HeadingGroup>
 
-	  <ContributionsWrapper>
-	    {
-	      contributors.map(contributor => (
-			<ContributorBubble key={contributor.id} href={contributor.html_url} target="_blank" title={`${contributor.login} \nContributions: ${contributor.contributions}`}>
-			  <img src={contributor.avatar_url} alt={contributor.login} />
-			</ContributorBubble>
-		  ))
-	    }
-	  </ContributionsWrapper>
-	</Wrapper>
-  ) : null;
+      <ContributionsWrapper>
+        {
+          contributors.map(contributor => (
+            <ContributorBubble key={contributor.id} href={contributor.html_url} target="_blank" title={`${contributor.login} \nContributions: ${contributor.contributions}`}>
+              <img src={contributor.avatar_url} alt={contributor.login} />
+            </ContributorBubble>
+          ))
+        }
+      </ContributionsWrapper>
+    </Wrapper>
+   ) : null;
 };
 
 export default Contributors;
