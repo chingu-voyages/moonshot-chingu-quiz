9798809a6d9b01295eb2aded0d263ae1c631f2fd
Regina M <28935544+rmcguire6@users.noreply.github.com>
2021-04-21 09:54:15 -0700

    Revert "Add contributors section"

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Hero  from "../components/aboutUs/HeroSection";
 import Info  from "../components/aboutUs/InfoSection";
-import Contributors  from "../components/aboutUs/Contributors";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Info />
-      <Contributors />
     </>
   );
 }
