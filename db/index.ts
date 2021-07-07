6974e37c26b9c694d9ae6752dd19c3d4ec3cded2
Jay T <jaytula@gmail.com>
2021-07-06 18:18:43 -0700

    Export postgres Pool object for more flexibility

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 import config   from "./config";
 
-const pool = new Pool(config);
+export const pool = new Pool(config);
 
 export default {
   async query(text: string, params?: string[]) {
